3eaf1b46880066b6f625a3187ff88203235f75b5
kunal00000 <kunalverma2468@gmail.com>
2024-04-14 21:02:10 +0530

    fix temp issue

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,9 +3,9 @@ import { cors } from 'hono/cors';
 import { handle } from 'hono/vercel';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import { TOPICS } from '@/lib/constants';
-import { generateScriptForPodcast } from '@/services/create-podcast';
-import { convertScriptToAudio } from '@/services/audio';
+// import { TOPICS } from '@/lib/constants';
+// import { generateScriptForPodcast } from '@/services/create-podcast';
+// import { convertScriptToAudio } from '@/services/audio';
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 
@@ -15,34 +15,34 @@ const app = new Hono().basePath('/api');
 
 app.use('/*', cors());
 
-app.post('create-episode', async (c)=>{
-  const {topic, count} = await c.req.json()
-  if(!topic || !count){
-    return c.json({message: 'Topic and count are required'}, {status: 400});
-  }
-  if(!TOPICS.includes(topic)){
-    return c.json({message: 'Invalid topic'}, {status: 400});
-  }
+// app.post('create-episode', async (c)=>{
+//   const {topic, count} = await c.req.json()
+//   if(!topic || !count){
+//     return c.json({message: 'Topic and count are required'}, {status: 400});
+//   }
+//   if(!TOPICS.includes(topic)){
+//     return c.json({message: 'Invalid topic'}, {status: 400});
+//   }
 
-  const filename = `${topic}-${Date.now()}`;
+//   const filename = `${topic}-${Date.now()}`;
 
-  const episode = await prisma.episode.create({
-    data: {
-      filename,
-      podcastType: topic as string
-    }
-  });
+//   const episode = await prisma.episode.create({
+//     data: {
+//       filename,
+//       podcastType: topic as string
+//     }
+//   });
 
-  const ssml_script = await generateScriptForPodcast(count, episode.id, topic);
+//   const ssml_script = await generateScriptForPodcast(count, episode.id, topic);
 
-  if(!ssml_script){
-    return c.json({message: 'Failed to generate script for podcast'}, {status: 500});
-  }
+//   if(!ssml_script){
+//     return c.json({message: 'Failed to generate script for podcast'}, {status: 500});
+//   }
 
-  await convertScriptToAudio(ssml_script, filename, episode.id);
+//   await convertScriptToAudio(ssml_script, filename, episode.id);
 
-  return c.json({message: 'success', data: episode.id}, {status: 200});
-})
+//   return c.json({message: 'success', data: episode.id}, {status: 200});
+// })
 
 app.post('/waitlist', async (c) => {
   const { name, email } = await c.req.json();
